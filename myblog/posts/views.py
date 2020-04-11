c64d593a38e2bf5f30e59e307575fe1bb6279f87
shaurya sinha <shaurya.r.sinha2000@gmail.com>
2020-04-11 12:48:56 +0530

    BLog post topic model created

--- myblog/posts/views.py
+++ myblog/posts/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from .models import Post
+
+def posts_list(request):
+    all_posts = Post.objects.all()
+    context = {
+        'all_posts' : all_posts
+    }
+    return render(request , "posts/posts_list.html" , context)
